e8c640b79d3df436550ffbbdfe9431379470b079
MarcusSlover <mariuszek016@gmail.com>
2021-01-01 05:17:32 +0100

    + items (not finished)

--- src/main/java/me/tecc/hypercode/templates/item/Item.java
+++ src/main/java/me/tecc/hypercode/templates/item/Item.java
@@ -1,14 +1,36 @@
 package me.tecc.hypercode.templates.item;
 
-public class Item {
-    String material;
-    short amount;
+import com.google.gson.JsonObject;
+import me.tecc.hypercode.utils.IJsonModel;
 
+/**
+ * Wrapper for Items from the DiamondFire templates format.
+ */
+public class Item implements IJsonModel<Item> {
+    String material; // E.g. "firework_rocket"
+    short amount;
     ItemMeta meta;
 
     public Item(String material, short amount, ItemMeta meta) {
         this.material = material;
         this.amount = amount;
         this.meta = meta;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setAmount(short amount) {
+        this.amount = amount;
+    }
+
+    public void setMeta(ItemMeta meta) {
+        this.meta = meta;
+    }
+
+    @Override
+    public JsonObject toJson(Item value) {
+        return null;
     }
 }
